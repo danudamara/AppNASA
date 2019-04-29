02e9ccac281018f8fa2a3c1b621dbae7c886ea4b
danudamara <48472598+danudamara@users.noreply.github.com>
2019-04-29 21:09:05 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=kMGEWs34hrRqJ9ehjM3FgSUzRLmY9YeJiYs1gFkr";
         
         $("#loadImage").click(function(){
             $.ajax({
